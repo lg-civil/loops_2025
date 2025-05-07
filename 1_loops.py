a10be1298106201838ab260442c5740d2c653c83
lg-civil <lgzuniga-urz@cps.edu>
2025-05-07 08:34:20 -0500

    I just worked with loops 2

--- 1_loops.py
+++ 1_loops.py
@@ -9,8 +9,8 @@ fruits = ["apple", "banana", "cherry", "date"]
 # print(fruits[2])
 for fruit in fruits:
     print(fruit)
+# I just worked with loops
 
-;;ll;;l;;
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
