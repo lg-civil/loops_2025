fd0df562ae876901962f61c26ada7ce74f00ea77
lg-civil <lgzuniga-urz@cps.edu>
2025-12-08 19:35:41 +0000

    loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,23 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+# for loops = execute a block of code a fixed number of times.
+#  you can iterate over a range, string, sequence, etc.
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
+
+
+name = input("Enter your name ")
+
+# while loop = execute some code WHILE some condition remains true
+
+# while name == '':
+ # print("You did not enter your name")
+
+# print(name)
+
+age =  int(input("Enter "))
